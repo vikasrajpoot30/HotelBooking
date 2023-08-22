a2121de3764bd36f504a8e00d3bad5e1b766043c
vikasrajpoot30 <vikas9rajpoot@gmail.com>
2023-08-22 22:35:44 +0530

    First commit

---
+++ client/src/utils/axios.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+    baseURL: 'http://localhost:4000',
+    // baseURL: "https://airbnb-api.up.railway.app",
+    withCredentials: true,
+})
+
+export default axiosInstance;
+
