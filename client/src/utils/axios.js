a9c2c5a40fd087a6b5f46af26087105251d5bc9f
Vikas Rajpoot <120569817+vikasrajpoot30@users.noreply.github.com>
2023-08-23 08:12:04 +0530

    Update axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:4000',
-    // baseURL: "https://airbnb-api.up.railway.app",
+    baseURL: "https://hotelbooking-production-114e.up.railway.app",
     withCredentials: true,
 })
 
